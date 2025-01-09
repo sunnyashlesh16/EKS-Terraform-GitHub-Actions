de1f568e3c9c0dfed8a78a2a934cc98fc75305e3
sunnyashlesh16 <85336488+sunnyashlesh16@users.noreply.github.com>
2025-01-09 17:46:19 -0500

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "dev-aman-tf-bucket"
+    bucket         = "sunny-tf-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "Lock-Files-Tf"
     encrypt        = true
   }
 }
